ec1c6d7a11e9b72c2847ecd59a44bb934171bec9
Enrique Garcia <engarciafranco@cometnet-10-21-2-45.utdallas.edu>
2018-10-28 19:54:25 -0500

    Updated app with optional stories, README updated to reflect new changes

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,17 +11,61 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var card: UIView!
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
     
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        card.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        frontLabel.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.4
+       
+        backLabel.clipsToBounds = true;
+        frontLabel.clipsToBounds = true
+        
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.4378522635, green: 0.6547383666, blue: 0.8708304763, alpha: 1)
+        btnOptionOne.layer.cornerRadius = 15.0
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.4378522635, green: 0.6547383666, blue: 0.8708304763, alpha: 1)
+        btnOptionTwo.layer.cornerRadius = 15.0
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.4378522635, green: 0.6547383666, blue: 0.8708304763, alpha: 1)
+        btnOptionThree.layer.cornerRadius = 15.0
     }
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+//        frontLabel.isHidden = true
+        if (frontLabel.isHidden == true) {
+                frontLabel.isHidden = false;
+                btnOptionOne.isHidden = false;
+                btnOptionThree.isHidden = false;
+                btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.4378522635, green: 0.6547383666, blue: 0.8708304763, alpha: 1)
+        }
+        
     }
+    
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        frontLabel.isHidden = true
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
+    }
+    
     
 }
 
--- README.md
+++ README.md
@@ -9,6 +9,17 @@ This flashcards app currently only has one flashcard which tells you what the ca
 
 <img src="http://g.recordit.co/xl0yUGbrWn.gif" width=200><br>
 
+<hr>
+
+UPDATE: 10-28-18 Version 0.1.0
+The Flashcard app now quizzes you by asking you to chose the correct answer between three options. 
+
+<img src="https://imgur.com/m1KqJUN.jpg" width=200><br>
+
+When you tap on the wrong answer, the respective button dissapears. If the correct answe is chosen, then the answer is revealed from behind the question card, and the correct answer is higlighted green. The user can also reset the question by clicking on the card.
+
+<img src="http://g.recordit.co/4DA0gWPHpL.gif" width=200><br>
+
 
 ## Required
 - [x] Create New Project in Xcode
@@ -17,6 +28,6 @@ This flashcards app currently only has one flashcard which tells you what the ca
 - [x] Build in logic to show the answer side when the card is tapped
 - [x] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
-- [ ] Add selectable multiple choice answers beneath the card
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
